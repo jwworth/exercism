3b8a7c412725e49f7dffdf959d923531f78578ec
Jake Worth <jake@jakeworth.com>
2016-11-08 10:54:05 -0600

    Add missing semicolons

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -4,16 +4,16 @@ Hamming.prototype.compute = function(strand1, strand2) {
   var hamming_distance = 0;
 
   if(strand1.length != strand2.length) {
-    throw 'DNA strands must be of equal length.'
+    throw 'DNA strands must be of equal length.';
   }
 
   for (i = 0; i < strand1.length; i++) {
     if(strand1[i] != strand2[i]) {
-      hamming_distance++
+      hamming_distance++;
     }
   }
 
   return hamming_distance;
-}
+};
 
 module.exports = Hamming;
