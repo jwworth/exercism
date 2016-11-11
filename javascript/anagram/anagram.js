e2ee04d8ae3a651daf00a563f562d8f5381edfd0
Jake Worth <jake@jakeworth.com>
2016-11-11 15:47:51 -0600

    Upgrade to a better JSHint config file

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -2,17 +2,21 @@
 
 var Anagram = function (comparator) {
 
-  this.anagram = function (comparator, comparision) {
-    var orderedComparator  = comparator.toLowerCase().split('').sort().join();
-    var orderedComparision = comparision.toLowerCase().split('').sort().join();
-    var notMatch           = comparator.toLowerCase() != comparision.toLowerCase();
+  this.order = function (word) {
+    return word.toLowerCase().split('').sort().join();
+  };
 
-    return (orderedComparator == orderedComparision && notMatch);
-  }
+  this.anagram = function (comparator, comparision) {
+    var orderedComparator  = this.order(comparator);
+    var orderedComparision = this.order(comparision);
+    var notMatch           = comparator.toLowerCase() !== comparision.toLowerCase();
+
+    return (orderedComparator === orderedComparision && notMatch);
+  };
 
   this.matches = function (comparisions) {
     return [].concat(comparisions).filter(comparision => this.anagram(comparator, comparision));
   };
-}
+};
 
 module.exports = Anagram;
