26484f96c7e6e37f9c6dcb36aeea7e489764306b
Jake Worth <jake@jakeworth.com>
2017-03-20 10:15:16 -0500

    Rename argument and use `each_char`

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,8 +2,8 @@ class Hamming
   def self.compute(first_strand, second_strand)
     raise ArgumentError if first_strand.length != second_strand.length
 
-    first_strand.split('').each_with_index.count do |strand, i|
-      second_strand[i] != strand
+    first_strand.each_char.with_index.count do |nucleotide, i|
+      second_strand[i] != nucleotide
     end
   end
 end
