4f1ef58210e1ab46901562b04a267360787c1661
Jake Worth <jake@jakeworth.com>
2017-05-04 13:03:50 -0500

    Don't use an unnecessary range

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -1,6 +1,6 @@
 class Binary
   def self.to_decimal(string)
-    raise ArgumentError if string.match?(/[^0-1]/)
+    raise ArgumentError if string.match?(/[^01]/)
 
     string.reverse.chars.map.with_index do |digit, index|
       digit.to_i * 2**index
