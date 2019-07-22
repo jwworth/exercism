6af2fff5a191fe9e34f4e1cb30541375fd486016
Jake Worth <jake@jakeworth.com>
2019-07-22 16:59:25 -0500

    Don't round early

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -9,36 +9,40 @@ class SpaceAge {
     return parseFloat(seconds.toFixed(2));
   }
 
+  convertToEarth(): number {
+    return this.seconds / 31557600;
+  }
+
   onEarth(): number {
-    return this.truncate(this.seconds / 31557600);
+    return this.truncate(this.convertToEarth());
   }
 
   onMercury(): number {
-    return this.truncate(this.onEarth() / 0.2408467);
+    return this.truncate(this.convertToEarth() / 0.2408467);
   }
 
   onVenus(): number {
-    return this.truncate(this.onEarth() / 0.61519726) - 0.01;
+    return this.truncate(this.convertToEarth() / 0.61519726);
   }
 
   onMars(): number {
-    return this.truncate(this.onEarth() / 1.8808158);
+    return this.truncate(this.convertToEarth() / 1.8808158);
   }
 
   onJupiter(): number {
-    return this.truncate(this.onEarth() / 11.862615);
+    return this.truncate(this.convertToEarth() / 11.862615);
   }
 
   onSaturn(): number {
-    return this.truncate(this.onEarth() / 29.447498);
+    return this.truncate(this.convertToEarth() / 29.447498);
   }
 
   onUranus(): number {
-    return this.truncate(this.onEarth() / 84.016846);
+    return this.truncate(this.convertToEarth() / 84.016846);
   }
 
   onNeptune(): number {
-    return this.truncate(this.onEarth() / 164.79132);
+    return this.truncate(this.convertToEarth() / 164.79132);
   }
 }
 
