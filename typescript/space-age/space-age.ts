24b188456ed2de48bb8d219e39018e5ffa332ee8
Jake Worth <jake@jakeworth.com>
2019-07-22 17:02:45 -0500

    Refactor

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,48 +1,46 @@
 class SpaceAge {
   seconds: number;
+  earthPeriod: number;
 
   constructor(seconds: number) {
     this.seconds = seconds;
+    this.earthPeriod = seconds / 31557600;
   }
 
   truncate(seconds: number): number {
     return parseFloat(seconds.toFixed(2));
   }
 
-  convertToEarth(): number {
-    return this.seconds / 31557600;
-  }
-
   onEarth(): number {
-    return this.truncate(this.convertToEarth());
+    return this.truncate(this.earthPeriod);
   }
 
   onMercury(): number {
-    return this.truncate(this.convertToEarth() / 0.2408467);
+    return this.truncate(this.earthPeriod / 0.2408467);
   }
 
   onVenus(): number {
-    return this.truncate(this.convertToEarth() / 0.61519726);
+    return this.truncate(this.earthPeriod / 0.61519726);
   }
 
   onMars(): number {
-    return this.truncate(this.convertToEarth() / 1.8808158);
+    return this.truncate(this.earthPeriod / 1.8808158);
   }
 
   onJupiter(): number {
-    return this.truncate(this.convertToEarth() / 11.862615);
+    return this.truncate(this.earthPeriod / 11.862615);
   }
 
   onSaturn(): number {
-    return this.truncate(this.convertToEarth() / 29.447498);
+    return this.truncate(this.earthPeriod / 29.447498);
   }
 
   onUranus(): number {
-    return this.truncate(this.convertToEarth() / 84.016846);
+    return this.truncate(this.earthPeriod / 84.016846);
   }
 
   onNeptune(): number {
-    return this.truncate(this.convertToEarth() / 164.79132);
+    return this.truncate(this.earthPeriod / 164.79132);
   }
 }
 
