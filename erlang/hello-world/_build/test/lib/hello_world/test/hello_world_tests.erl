c3117bfa39ee047d0cbb02bedc4060b573243d46
Jake Worth <jake@jakeworth.com>
2019-07-31 09:58:51 -0500

    Remove built artifacts

--- erlang/hello-world/_build/test/lib/hello_world/test/hello_world_tests.erl
+++
@@ -1,9 +0,0 @@
--module(hello_world_tests).
-
--define(TESTED_MODULE, (sut(hello_world))).
--define(TEST_VERSION, 2).
--include("exercism.hrl").
-
-
-say_hi_test() ->
-  ?assertEqual("Hello, World!", ?TESTED_MODULE:hello()).
