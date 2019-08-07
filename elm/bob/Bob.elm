0fa48457498a6f68d9a455a4f1b684eac4083699
Jake Worth <jake@jakeworth.com>
2019-08-07 08:10:53 -0500

    Rename methods

--- elm/bob/Bob.elm
+++ elm/bob/Bob.elm
@@ -11,24 +11,24 @@ hey remark =
         trimmed_remark =
             trim remark
     in
-    if isAllCaps trimmed_remark && isQuestion trimmed_remark then
+    if isYelling trimmed_remark && isQuestion trimmed_remark then
         "Calm down, I know what I'm doing!"
 
-    else if isAllCaps trimmed_remark then
+    else if isYelling trimmed_remark then
         "Whoa, chill out!"
 
     else if isQuestion trimmed_remark then
         "Sure."
 
-    else if isWhitespace trimmed_remark then
+    else if isSilence trimmed_remark then
         "Fine. Be that way!"
 
     else
         "Whatever."
 
 
-isAllCaps : String -> Bool
-isAllCaps str =
+isYelling : String -> Bool
+isYelling str =
     any isAlpha str
         && toUpper str
         == str
@@ -39,6 +39,6 @@ isQuestion str =
     endsWith "?" str
 
 
-isWhitespace : String -> Bool
-isWhitespace str =
+isSilence : String -> Bool
+isSilence str =
     all isEmpty (words str)
