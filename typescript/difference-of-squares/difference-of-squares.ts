98341b5791f1a61f6ba5714cef1d64dae4e3b262
Jake Worth <jake@jakeworth.com>
2019-08-14 09:08:15 -0500

    Don't set something we won't reuse

--- typescript/difference-of-squares/difference-of-squares.ts
+++ typescript/difference-of-squares/difference-of-squares.ts
@@ -1,13 +1,12 @@
 class Squares {
-  input: number[];
   squareOfSum: number;
   sumOfSquares: number;
   difference: number;
 
   constructor(input: number) {
-    this.input = [...Array(input + 1).keys()];
-    this.squareOfSum = this.input.reduce((acc, num) => acc + num, 0) ** 2;
-    this.sumOfSquares = this.input.reduce((acc, num) => acc + num ** 2, 0);
+    const numbers: number[] = [...Array(input + 1).keys()];
+    this.squareOfSum = numbers.reduce((acc, num) => acc + num, 0) ** 2;
+    this.sumOfSquares = numbers.reduce((acc, num) => acc + num ** 2, 0);
     this.difference = this.squareOfSum - this.sumOfSquares;
   }
 }
