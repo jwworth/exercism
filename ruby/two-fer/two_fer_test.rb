a5bf6bdc0a0265de0fbe2cf4629638ab1cf42a0f
Jake Worth <jake@jakeworth.com>
2020-07-15 15:21:11 -0400

    Add missing test

--- ruby/two-fer/two_fer_test.rb
+++ ruby/two-fer/two_fer_test.rb
@@ -6,7 +6,7 @@ require_relative 'two_fer'
 # Common test data version: 1.2.0 4fc1acb
 class TwoFerTest < Minitest::Test
   def test_no_name_given
-    # skip
+    assert_equal 'One for you, one for me.', TwoFer.two_fer
   end
 
   def test_a_name_given
