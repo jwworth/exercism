7e1681c9c9064776130aa72a5e799cbbc3dbf107
Jake Worth <jake@jakeworth.com>
2020-07-23 13:03:06 -0400

    Open test and file in Vim

--- bin/download-js.sh
+++ bin/download-js.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+# Print message
 echo What exercise would you like to download?
+
+# Get exercise
 read exercise
+
+# Download exercise
 exercism download --exercise=$exercise --track=javascript
+
+# Open test and files in Vim
+tmux send-keys -t -1 "vim -o javascript/$exercise/$exercise.js javascript/$exercise/$exercise.spec.js" Enter
+
+# Enter directory, install dependencies, run tests
 cd javascript/$exercise
 npm i && npm test -- --watch
