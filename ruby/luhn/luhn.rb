b3aa4c04c95e94b3d926599e913b4cc37a025823
Jake Worth <jake@jakeworth.com>
2020-12-18 15:37:40 -0500

    Refactor

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -6,16 +6,14 @@ module Luhn
     return false unless sanitized =~ /^[0-9]{2,}$/
 
     result = sanitized.chars.reverse.each_with_index.reduce(0) do |acc, (char, index)|
-      integer = char.to_i
+      single = char.to_i
 
-      if index.even?
-        addition = integer
-      else
-        double = integer * 2
-        addition = (double > 9 ? double - 9 : double)
+      if index.odd?
+        double = single * 2
+        single = (double > 9 ? double - 9 : double)
       end
 
-      acc += addition
+      acc + single
     end
 
     (result % 10).zero?
