9ed195eb5afda28094a0a3fae27fb496cf6762d7
Jake Worth <jake@jakeworth.com>
2020-12-21 19:00:53 -0500

    Complete 'Scrabble' in Ruby

---
+++ ruby/scrabble-score/scrabble_score.rb
@@ -0,0 +1,27 @@
+class Scrabble
+  attr_reader :score
+
+  def initialize(input)
+    @score = Scorer.calculate(input)
+  end
+
+  def self.score(input)
+    Scorer.calculate(input)
+  end
+end
+
+SCORES = { a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, r: 1, s: 1, t: 1,
+           d: 2, g: 2, b: 3, c: 3, m: 3, p: 3, f: 4, h: 4, v: 4, w: 4,
+           y: 4, k: 5, j: 8, x: 8, q: 10, z: 10 }.freeze
+
+module Scorer
+  extend self
+
+  def calculate(input)
+    return 0 if input.nil? || input.strip.empty?
+
+    input.downcase.chars.inject(0) do |acc, char|
+      acc + SCORES[char.to_sym]
+    end
+  end
+end
