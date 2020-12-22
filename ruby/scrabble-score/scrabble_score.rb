1b832698221076a5b7549ccd6c3533b5bee8d96b
Jake Worth <jake@jakeworth.com>
2020-12-21 19:08:31 -0500

    Refactor

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,23 +1,19 @@
 class Scrabble
-  attr_reader :score
+  attr_reader :input
+
+  SCORES = { a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, r: 1, s: 1, t: 1,
+             d: 2, g: 2, b: 3, c: 3, m: 3, p: 3, f: 4, h: 4, v: 4, w: 4,
+             y: 4, k: 5, j: 8, x: 8, q: 10, z: 10 }.freeze
 
   def initialize(input)
-    @score = Scorer.calculate(input)
+    @input = input
   end
 
   def self.score(input)
-    Scorer.calculate(input)
+    new(input).score
   end
-end
 
-SCORES = { a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, r: 1, s: 1, t: 1,
-           d: 2, g: 2, b: 3, c: 3, m: 3, p: 3, f: 4, h: 4, v: 4, w: 4,
-           y: 4, k: 5, j: 8, x: 8, q: 10, z: 10 }.freeze
-
-module Scorer
-  extend self
-
-  def calculate(input)
+  def score
     return 0 if input.nil? || input.strip.empty?
 
     input.downcase.chars.inject(0) do |acc, char|
