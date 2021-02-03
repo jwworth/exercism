6872e11013eb1d8f691ec1c3c50b1864227473e7
Jake Worth <jake@jakeworth.com>
2021-02-03 13:59:57 -0500

    Complete 'atbash' in Ruby

---
+++ ruby/atbash-cipher/atbash_cipher.rb
@@ -0,0 +1,39 @@
+module Atbash
+  def self.encode(plaintext)
+    result = prepare_encode(plaintext).map do |char|
+      if index = letters.index(char)
+        letters.reverse[index]
+      else
+        char
+      end
+    end.join
+
+    chunks_of_five(result)
+  end
+
+  def self.decode(cipher)
+    prepare_decode(cipher).map do |char|
+      if index = letters.reverse.index(char)
+        letters[index]
+      else
+        char
+      end
+    end.join
+  end
+
+  def self.prepare_decode(cipher)
+    cipher.gsub(/[^a-z0-9]/, '').chars
+  end
+
+  def self.prepare_encode(plaintext)
+    plaintext.downcase.gsub(/[^a-z0-9]/, '').chars
+  end
+
+  def self.letters
+    ('a'..'z').to_a
+  end
+
+  def self.chunks_of_five(encoded)
+    encoded.scan(/.{1,5}/).join(' ')
+  end
+end
