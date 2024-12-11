2c1720170a84d0e157448b2d9e426777cfdb1bb6
Jake Worth <jake@jakeworth.com>
2024-12-11 10:35:53 -0500

    Refactor some more

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -2,24 +2,26 @@ class Luhn:
     def __init__(self, card_num):
         self.card_num = card_num
 
-    def transform_digit(self, index, digit):
-      if index % 2 != 0:
-        doubled = digit * 2
-        if doubled > 9:
-          doubled = doubled -9
-        return doubled
+    def _transform_digit(self, index, digit):
+        if index % 2 != 0:
+            doubled = digit * 2
+            if doubled > 9:
+                doubled = doubled - 9
+            return doubled
 
-      else:
-        return digit
+        else:
+            return digit
 
     def valid(self):
-        stripped = self.card_num.replace(' ', '')
+        stripped = self.card_num.replace(" ", "")
 
         if not stripped.isnumeric() or len(stripped) < 2:
             return False
 
         digits = map(int, list(stripped))
-        transformed = (self.transform_digit(index, digit)
-                          for index, digit in enumerate(reversed(list(digits))))
+        transformed = (
+            self._transform_digit(index, digit)
+            for index, digit in enumerate(reversed(list(digits)))
+        )
 
         return sum(transformed) % 10 == 0
